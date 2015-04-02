4ce53acc04506dde13dc0f2572030ddaa8928dc7
HANNES PAULSSON <hannes.paulsson@gmail.com>
2015-04-02 09:06:37 +0200

    Added basic test functions

--- Motorfunctions.cpp
+++ Motorfunctions.cpp
@@ -5,12 +5,16 @@
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+
+pinMode(13, OUTPUT);
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
-	//Heeelloo
+digitalWrite(13, HIGH);
+delay(1000);
+digitalWrite(13, LOW);
+delay(100);
 }
