7ed5702fb8e964f269946db94f0e4f2166b17f26
Rodriguez <ramonnr@kth.se>
2015-04-02 13:11:39 +0200

    actual motor code

--- Motorfunctions.cpp
+++ Motorfunctions.cpp
@@ -1,25 +1,35 @@
 // Do not remove the include below
 #include "Motorfunctions.h"
 
+#define enable 9
+#define dir1 7
+#define dir2 8
+
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-Serial.begin(9600);
-pinMode(13, OUTPUT);
-
+pinMode(enable, OUTPUT);
+pinMode(dir1,OUTPUT);
+pinMode(dir2,OUTPUT);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-Serial.print("this is stuff");
-digitalWrite(13, HIGH);
-delay(1000);
-digitalWrite(13, LOW);
-delay(100);
-//här händer det grejor
-//this is a comment by R
-//Mickes commit
-//motherfucking commits
+
+	digitalWrite(dir1,LOW);
+	digitalWrite(dir2,HIGH);
+	analogWrite(enable,255);
+	delay(1000);
+	analogWrite(enable,0);
+	digitalWrite(dir2,LOW);
+	digitalWrite(dir1,HIGH);
+	delay(500);
+	analogWrite(enable,255);
+	delay(1000);
+	analogWrite(enable,0);
+	delay(500);
+
+
 }
