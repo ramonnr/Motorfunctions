0415ef640d16b876f77a03a389343ba4b95a5f72
Rodriguez <ramonnr@kth.se>
2015-04-02 10:09:59 +0200

    serial print

--- Motorfunctions.cpp
+++ Motorfunctions.cpp
@@ -5,7 +5,7 @@
 //The setup function is called once at startup of the sketch
 void setup()
 {
-
+Serial.begin(9600);
 pinMode(13, OUTPUT);
 
 }
@@ -13,6 +13,7 @@ pinMode(13, OUTPUT);
 // The loop function is called in an endless loop
 void loop()
 {
+Serial.print("this is stuff");
 digitalWrite(13, HIGH);
 delay(1000);
 digitalWrite(13, LOW);
