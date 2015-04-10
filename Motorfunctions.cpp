029fe71f35e978508b2738a4b5aedbee6e1baaa2
Micke <miandre@kth.se>
2015-04-10 15:38:38 +0200

    Accual test of DC-motor. Works with motor control shield.

--- Motorfunctions.cpp
+++ Motorfunctions.cpp
@@ -1,50 +1,42 @@
 // Do not remove the include below
 #include "Motorfunctions.h"
 
-#define enable 9
-#define dir1 7
-#define dir2 8
+#define dir 2
+#define pwm 3
 
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
 //pinMode(enable, OUTPUT);
-pinMode(dir1,OUTPUT);
-pinMode(dir2,OUTPUT);
+pinMode(dir,OUTPUT);
+pinMode(pwm,OUTPUT);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
 
-	digitalWrite(dir1,LOW);
-	delay(100);
-	digitalWrite(dir2,HIGH);
-	//analogWrite(enable,255);
-	delay(1000);
-	//analogWrite(enable,0);
-
-	digitalWrite(dir1,LOW);
-	delay(100);
-	digitalWrite(dir2,LOW);
-	delay(500);
-
-	//digitalWrite(dir2,LOW);
-	//delay(100);
-	digitalWrite(dir1,HIGH);
-	delay(1000);
-
-	digitalWrite(dir1,LOW);
-	delay(100);
-	digitalWrite(dir2,LOW);
-	delay(500);
-
-
-	//analogWrite(enable,255);
-	//delay(1000);
-	//analogWrite(enable,0);
-	//delay(500);
-
+//	for(int i=40; i<255; i+=20){
+//	digitalWrite(dir,HIGH);
+//	delay(100);
+//	analogWrite(pwm,i);
+//	delay(700);
+//	analogWrite(pwm,0);
+//delay(2000);
+//	digitalWrite(dir,LOW);
+//	delay(100);
+//	analogWrite(pwm,i);
+//	delay(700);
+//	analogWrite(pwm,0);
+//	delay(2000);
+//	}
+//	analogWrite(pwm,0);
+//	delay(100000000);
+	digitalWrite(dir,HIGH);
+	analogWrite(pwm,255);
+	delay(2000);
+	analogWrite(pwm,0);
+		delay(200000000);
 
 }
