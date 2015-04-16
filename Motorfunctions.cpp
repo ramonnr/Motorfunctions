70fd921ae724ba1aa86e2c92a004e5d8f03d332f
Micke <miandre@kth.se>
2015-04-16 16:32:37 +0200

    Bluetooth + servo test

--- Motorfunctions.cpp
+++ Motorfunctions.cpp
@@ -1,42 +1,73 @@
 // Do not remove the include below
 #include "Motorfunctions.h"
+#include "Servo.h"
 
 #define dir 2
-#define pwm 3
+#define pwm 10
+char inputArray[2];
+uint8_t angle;
+Servo servo;
 
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-//pinMode(enable, OUTPUT);
-pinMode(dir,OUTPUT);
-pinMode(pwm,OUTPUT);
+	//pinMode(enable, OUTPUT);
+	pinMode(dir,OUTPUT);
+	//pinMode(pwm,OUTPUT);
+	pinMode(13, OUTPUT);
+	Serial.begin(9600);
+	Serial.println("Initializing! New");
+servo.attach(11);
+	servo.write(70);
+	delay(300);
+	servo.write(90);
+	delay(300);
+	servo.write(110);
+	delay(1000);
+	servo.write(90);
+	delay(1000);
+
+
 }
+
 
 // The loop function is called in an endless loop
 void loop()
 {
 
-//	for(int i=40; i<255; i+=20){
-//	digitalWrite(dir,HIGH);
-//	delay(100);
-//	analogWrite(pwm,i);
-//	delay(700);
-//	analogWrite(pwm,0);
-//delay(2000);
-//	digitalWrite(dir,LOW);
-//	delay(100);
-//	analogWrite(pwm,i);
-//	delay(700);
-//	analogWrite(pwm,0);
-//	delay(2000);
-//	}
-//	analogWrite(pwm,0);
-//	delay(100000000);
-	digitalWrite(dir,HIGH);
-	analogWrite(pwm,255);
-	delay(2000);
-	analogWrite(pwm,0);
-		delay(200000000);
+	if(Serial.available()>0){
+		Serial.readBytes(inputArray,2);
 
+		angle= (inputArray[1]);
+		servo.write(angle);
+		Serial.end();
+		Serial.begin(9600);
+
+	}
+
+
+
+	//	for(int i=40; i<255; i+=20){
+	//	digitalWrite(dir,HIGH);
+	//	delay(100);
+	//	servo.write(pwm,i);
+	//	delay(700);
+	//	servo.write(pwm,0);
+	//delay(2000);
+	//	digitalWrite(dir,LOW);
+	//	delay(100);
+	//	servo.write(pwm,i);
+	//	delay(700);
+	//	servo.write(pwm,0);
+	//	delay(2000);
+	//	}
+	//	servo.write(pwm,0);
+	//	delay(100000000);
+	//	digitalWrite(dir,HIGH);
+	//	servo.write(pwm,255);
+	//	delay(2000);
+	//	servo.write(pwm,0);
+	//		delay(200000000);
 }
+
